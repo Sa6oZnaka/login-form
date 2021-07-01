95c1a62b7734a49cd5e977d63e08b959ffb40907
Alexandar Karapenev <treildr@gmail.com>
2021-07-01 15:59:01 +0300

    Email is now sent after regitration

--- passport.js
+++ passport.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 let LocalStrategy = require("passport-local").Strategy,
     bcrypt = require('bcrypt-nodejs');
 
@@ -45,11 +47,12 @@ module.exports = function (passport, connection) {
                                     //console.log(rows.insertId);
                                     newUserMysql.id = rows.insertId;
 
+                                    sendMail(username, verification_token).catch(console.error);
                                     return done(null, newUserMysql);
                                 });
                             }
                             // send verification token mail
-
+                            
                     });
             })
     );
@@ -89,3 +92,35 @@ function generateToken(length) {
    }
    return result;
 }
+
+
+async function sendMail(receaver, token) {
+
+    let testAccount = await nodemailer.createTestAccount();
+  
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+  
+
+    let info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: receaver, // list of receivers
+      subject: "Confirm your registration", // Subject line
+      text: "Here is your token: " + token, // plain text body
+      html: "<b>Hello</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
